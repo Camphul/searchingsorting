02432f14d7d7c5ba0677ad8e6404046d8b411c04
JdeJong <jeroen--j@hotmail.com>
2019-11-29 13:10:23 +0100

    skrrrt

--- sands/src/main/java/nl/hva/ict/se/sands/BackwardsSearch.java
+++ sands/src/main/java/nl/hva/ict/se/sands/BackwardsSearch.java
@@ -3,18 +3,54 @@ package nl.hva.ict.se.sands;
 public class BackwardsSearch {
 
     private int comparisonCount = 0;
+    private int R;     // the radix
+    private int[] left;     // the bad-character skip array
+
+    private String needle;  // store the pattern as a character array
+    private String haystack;
     /**
      * Returns index of the right most location where <code>needle</code> occurs within <code>haystack</code>. Searching
-     * starts at the right end side of the text (<code>haystack</code>) and proceeds to the first character (left side).
+     * starts at the right end side of the text (<code>haystack</code>) and proceeds to the first character (right side).
      * @param needle the text to search for.
      * @param haystack the text which might contain the <code>needle</code>.
-     * @return -1 if the <code>needle</code> is not found and otherwise the left most index of the first
+     * @return -1 if the <code>needle</code> is not found and otherwise the right most index of the first
      * character of the <code>needle</code>.
      */
     int findLocation(String needle, String haystack) {
         comparisonCount = 0;
+        this.needle = needle;
+        this.haystack = haystack;
 
-        return -1;
+        this.R = 256;
+
+        // preprocess pattern ----------------------------------------
+        // position of rightmost occurrence of c in the pattern
+        left = new int[R];
+        for (int c = R-1; c > 0; c--)
+            left[c] = needle.length();
+        for (int j = needle.length() - 1; j > 0; j--)
+            left[needle.charAt(j)] = j;
+
+        // search ----------------------------------------------------
+        int m = needle.length();
+        int n = haystack.length();
+        int skip = 0;
+        for (int i = n - m; i >= m; i -= skip) {
+            System.out.println("skip was " + skip + ", now looking at text index " + i);
+            skip = 0;
+            for (int j = 0; j < m; j++) {
+                this.comparisonCount++;
+                if (needle.charAt(j) != haystack.charAt(i+j)) {
+                    skip = Math.max(1, j + left[haystack.charAt(i+j)]);
+                    break;
+                }
+            }
+            if (skip == 0) return i;    // found
+        }
+        return -1; // not found
+    }
+
+    public BackwardsSearch() {
     }
 
     /**
--- sands/src/test/java/nl/hva/ict/se/sands/BackwardsSearchTest.java
+++ sands/src/test/java/nl/hva/ict/se/sands/BackwardsSearchTest.java
@@ -3,6 +3,8 @@ package nl.hva.ict.se.sands;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BackwardsSearchTest {
@@ -32,5 +34,22 @@ public class BackwardsSearchTest {
         searchEngine.findLocation("needle", "whereistheneedle");
 
         assertEquals(6, searchEngine.getComparisonsForLastSearch());
+    }
+
+    @Test
+    public void compareWithLargeText() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 100000;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+
     }
 }
